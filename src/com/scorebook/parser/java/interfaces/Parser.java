ecca96429deccbbc2ed8a1eddb0260035035ba0b
Jeff Stokes <jeff@alumni.ecu.edu>
2011-09-29 16:33:37 -0400

    interfaces didn't get added

---
+++ src/com/scorebook/parser/java/interfaces/Parser.java
@@ -0,0 +1,11 @@
+package com.scorebook.parser.java.interfaces;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: jstokes4
+ * Date: 9/29/11
+ * Time: 4:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Parser {
+}
