ec2173bdec1076a5f6b9e344c85e3f552997485a
Jeff Stokes <stokesj07@students.ecu.edu>
2011-10-03 18:10:56 -0400

    trying to get sbt working

--- src/com/sportsbook/parser/IParser.java
+++
@@ -1,9 +0,0 @@
-package com.sportsbook.parser;
-
-import com.sportsbook.parser.java.Game;
-
-public interface IParser {
-    String source =  null;
-    Game[] parse();
-    String readSource(); //for testing
-}
