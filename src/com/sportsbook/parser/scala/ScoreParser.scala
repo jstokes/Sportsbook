540e065ec40874b0018950e166ea5d3d81da8df2
Jeff Stokes <stokesj07@students.ecu.edu>
2011-10-03 12:22:53 -0400

    committing build.xml

--- src/com/sportsbook/parser/scala/ScoreParser.scala
+++ src/com/sportsbook/parser/scala/ScoreParser.scala
@@ -1,6 +1,21 @@
 package com.sportsbook.parser.scala
-import com.sportsbook.parser.java.interfaces.IParser
 
-class ScoreParser extends IParser{
+import _root_.java.net.{URL, URLConnection, URLConnection}
+import com.sportsbook.parser.java.{Game, League, Team}
+import com.sportsbook.parser.IParser
+import _root_.scala.xml._
+
+class ScoreParser extends IParser {
+   def getSource:Elem = {
+      import java.net.{URLConnection, URL}
+      val source = """"http://scores.espn.go.com/nfl/scoreboard"""
+      val url = new URL(source)
+      val connection:URLConnection = url.openConnection
+      XML.load(connection.getInputStream)
+   }
    def parse = null
+
+   def main(args:Array[String]) {
+      println(getSource)
+   }
 }
