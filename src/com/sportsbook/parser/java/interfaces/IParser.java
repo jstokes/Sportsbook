865cd432947ce9e086173e13598b03982efc30b5
Jeff Stokes <stokesj07@students.ecu.edu>
2011-10-03 00:06:23 -0400

    scoreparser

--- src/com/sportsbook/parser/java/interfaces/IParser.java
+++ src/com/sportsbook/parser/java/interfaces/IParser.java
@@ -1,6 +1,6 @@
 package com.sportsbook.parser.java.interfaces;
 
-import com.sportsbook.parser.java.Game;
+import com.sportsbook.parser.Game;
 
 public interface IParser {
     String source =  null;
